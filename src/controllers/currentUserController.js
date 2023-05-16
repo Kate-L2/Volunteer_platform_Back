06d47489e5b5346aaab1a3ebc6271a6fb4cd1e11
Lobzzek <lopatinskiden@gmail.com>
2023-05-16 21:30:19 +0300

    fix: remove extra code for currentUser

--- src/controllers/currentUserController.js
+++ src/controllers/currentUserController.js
@@ -1,16 +1,10 @@
 import tryCatch from "./utils/tryCatch.js";
 
 const currentUser = tryCatch(async (req, res) => {
-  if (!req.user) {
-    res.status(404).send({
-      message: "User doesn`t exist",
-    });
-  } else {
-    res.status(200).send({
-      message: "Current user successful",
-      result: req.user,
-    });
-  }
+  return res.status(200).send({
+    message: "Current user successful",
+    result: req.user,
+  });
 });
 
 export default currentUser;
