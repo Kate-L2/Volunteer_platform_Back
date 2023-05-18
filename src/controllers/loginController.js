a7a9c896395c1bebb97fc3b72af14895a908d049
Kate <zlatovlaskaya27@gmail.com>
2023-05-18 15:09:32 +0200

    loginController and verifuJWT changes

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -4,7 +4,6 @@ import tryCatch from "./utils/tryCatch.js";
 import generateJwt from "./utils/generateJwt.js";
 import parseUser from "./utils/parseUser.js";
 
-const maxAge = 1000 * 60 * 60 * 24;
 
 const login = tryCatch(async (req, res) => {
   const { email, password } = req.body;
@@ -23,39 +22,9 @@ const login = tryCatch(async (req, res) => {
             message: "Passwords does not match",
           });
         }
-<<<<<<< HEAD
-        const accessToken = jwt.sign(
-          {
-            userId: user._id,
-            email: user.email,
-            role: user.role,
-          },
-          process.env.ACCESS_TOKEN_SECRET,
-          { expiresIn: "30s" }
-        );
-        const refreshToken = jwt.sign(
-          {
-            userId: user._id,
-            email: user.email,
-            role: user.role,
-          },
-          process.env.REFRESH_TOKEN_SECRET,
-          { expiresIn: "1h" }
-        );
-        user.refreshToken = refreshToken;
-        user.save();
-        console.log(user);
-        res.cookie("jwt", refreshToken, {
-          httpOnly: true,
-          secure: true,
-          sameSite: "None",
-          maxAge,
-        });
-=======
         const userCopy = parseUser(user);
         const token = generateJwt(userCopy);
 
->>>>>>> 6476cea0153302853961c545168f291b6be834bc
         res.status(200).send({
           message: "Login Successful",
           result: { ...userCopy, token },
--- src/middleware/verifyJWT.js
+++ src/middleware/verifyJWT.js
@@ -1,20 +1,4 @@
 import jwt from "jsonwebtoken";
-<<<<<<< HEAD
-
-const verifyJWT = (req, res, next) => {
-  const authHeader = req.headers.authorization || req.headers.Authorization;
-
-  if (!authHeader?.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-
-  const token = authHeader.split(" ")[1];
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) return res.status(403).json({ message: "Forbidden" });
-    req.user = decoded.UserInfo.email;
-    req.role = decoded.UserInfo.roles;
-=======
 import * as dotenv from "dotenv";
 import User from "../models/User.js";
 import parseUser from "../controllers/utils/parseUser.js";
@@ -26,7 +10,6 @@ const verifyJWT = async (req, res, next) => {
 
   if (!token) {
     req.user = null;
->>>>>>> 6476cea0153302853961c545168f291b6be834bc
     next();
     return;
   }
